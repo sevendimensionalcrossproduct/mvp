8b37c28da36e8205e6a28f4c614774f6c3f93da1
sevendimensionalcrossproduct <daim170743@gmail.com>
2024-10-28 06:50:47 -0700

    react hook use state bs

--- Web Design/Ruining My Life React Edition/Babel Edition/cock.js
+++ Web Design/Ruining My Life React Edition/Babel Edition/cock.js
@@ -1,6 +1,18 @@
-function LeButtion() {
-  return /*#__PURE__*/React.createElement("button", null, "penor");
+const {
+  useState
+} = React;
+function LeButtion({
+  count,
+  setCount
+}) {
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "penor");
 }
 function penis() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, " ", /*#__PURE__*/React.createElement("p", null, "ur mom"), /*#__PURE__*/React.createElement(LeButtion, null));
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, " ", /*#__PURE__*/React.createElement("p", null, "ur mom"), /*#__PURE__*/React.createElement(LeButtion, {
+    count: count,
+    setCount: setCount
+  }), /*#__PURE__*/React.createElement("p", null, "le cound is ", count));
 }
--- Web Design/Ruining My Life React Edition/Babel Edition/cock.jsx
+++ Web Design/Ruining My Life React Edition/Babel Edition/cock.jsx
@@ -1,12 +1,17 @@
-function LeButtion(){
-    return <button>penor</button>
+const { useState } = React;
+
+function LeButtion({ count, setCount }){
+    return <button onClick={() => setCount(count+1)}>penor</button>
 }
 
 function penis(){
+    const [count, setCount] = React.useState(0);
     return (
         <> {/* fragment to not have a shitload of pointless wrapping divs*/}
+            
             <p>ur mom</p>
-            <LeButtion/>
+            <LeButtion count={count} setCount={setCount}/>
+            <p>le cound is {count}</p>
         </>
     );
 }
